c9f2acfe5235db5e427898cc0e479aabaf669382
Patrick Thomson <patrickt@github.com>
2018-04-30 10:11:12 -0400

    Initial Commit

---
+++ Shades/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Shades
+//
+//  Created by Patrick William Thomson on 4/30/18.
+//  Copyright © 2018 Patrick Thomson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
